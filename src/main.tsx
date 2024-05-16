3cc95c7777916698344792d4632964ea655b0395
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2024-05-17 02:18:54 +0300

    final edits 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
@@ -8,18 +6,13 @@ import MainLoader from "./components/MainLoader.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
 import "./index.css";
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Toaster />
-    <QueryClientProvider client={queryClient}>
-      <HelmetProvider>
-        <Suspense fallback={<MainLoader />}>
-          <App />
-        </Suspense>
-      </HelmetProvider>
-      <ReactQueryDevtools initialIsOpen={false} position="left" />
-    </QueryClientProvider>
+    <HelmetProvider>
+      <Suspense fallback={<MainLoader />}>
+        <App />
+      </Suspense>
+    </HelmetProvider>
   </React.StrictMode>
 );
